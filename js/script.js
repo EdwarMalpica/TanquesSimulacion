5733b537cc02cc9a3a575d7867aef6dacc30d000
Edwar Malpica <edwar.malpica@uptc.edu.co>
2022-06-11 21:58:38 -0500

    Animacion tanque 1

--- js/script.js
+++ js/script.js
@@ -19,15 +19,16 @@ let alturaH1H2= document.getElementById("diametroAgujeroH2");
 let anchorH1H2= document.getElementById("diametroAgujeroH2");
 const imgWaterKry = new Image();
 let resultados = document.getElementById("resultados");
-
-
+let volumenTanque1;
+let volumenTanque2;
+let intervalo;
 
 
 
 function drawTanque(tanque, dezplazamiento, altura=50, diametro = 50, diametroAgujero = 5,alturaAgujero=10, nivelAgua=30){
 
     if(tanque.getContext){
-        
+        //console.log(`altura: ${altura} diametro ${diametro} diamettro Aguja ${diametroAgujero} altura Agujero ${alturaAgujero} nivelAgua ${nivelAgua}`);
         let tanCa = tanque.getContext('2d');
            
         tanCa.beginPath();
@@ -36,7 +37,7 @@ function drawTanque(tanque, dezplazamiento, altura=50, diametro = 50, diametroAg
         tanCa.lineTo(diametro+50+dezplazamiento,altura+50);
         tanCa.lineTo(diametro+50+dezplazamiento,50);
         tanCa.fillStyle = "blue";
-        tanCa.fillRect(51+dezplazamiento,51+(50-nivelAgua),diametro-1,nivelAgua-1);
+        tanCa.fillRect(51+dezplazamiento,51+(50-nivelAgua)+(altura-50),diametro-1,nivelAgua-1);
         tanCa.fillStyle = 'gray';
         tanCa.fillRect(51+dezplazamiento+diametro,50+altura-diametroAgujero-alturaAgujero,5,diametroAgujero);
         //tanCa.closePath();
@@ -45,40 +46,122 @@ function drawTanque(tanque, dezplazamiento, altura=50, diametro = 50, diametroAg
     }
 }
 
+function initialValues(){
+    alturaH1.value = 50;
+    alturaH2.value = 50;
+    diametroH1.value = 50;
+    diametroH2.value = 50;
+    diametroAgujeroH1.value = 5;
+    diametroAgujeroH2.value = 5;
+    alturaAgujeroH1.value = 5;
+    alturaAgujeroH2.value = 5;
+    nivelH1.value = 20;
+    nivelH2.value = 20;
+}
+initialValues();
 function startApp(){
-    let radioH1 = diametroH1.value/2;
+    alturaH1 = parseFloat(alturaH1.value);
+    alturaH2 = parseFloat(alturaH2.value);
+    diametroH1 = parseFloat(diametroH1.value);
+    diametroH2 = parseFloat(diametroH2.value);
+    diametroAgujeroH1 = parseFloat(diametroAgujeroH1.value);
+    diametroAgujeroH2 = parseFloat(diametroAgujeroH2.value);
+    alturaAgujeroH1 = parseFloat(alturaAgujeroH1.value);
+    alturaAgujeroH2 = parseFloat(alturaAgujeroH2.value);
+    nivelH1 = parseFloat(nivelH1.value);
+    nivelH2 = parseFloat(nivelH2.value);
+
+    intervalo = setInterval(starSimulation,10);
+    
+
+    /*let radioH1 = diametroH1.value/2;
     let raudioAgujeroH1 = diametroAgujeroH1.value/2;
     let h1n1 = nivelH1.value-alturaAgujeroH1.value;
-    calculateCaudal1(raudioAgujeroH1,h1n1,radioH1);
+    calculateCaudal1(raudioAgujeroH1,h1n1,radioH1);*/
 }
-function calculateCaudal(){
-    let volumenTanque1 = Math.pi * Math.pow(diametroH1.value/2,2)*nivelH1.value;
-    let caudal = 0.6 * (Math.PI* Math.pow(diametroAgujeroH1.value/2,2)* Math.sqrt(2*9.8*(nivelH1.value-alturaAgujeroH1.value)));
-    console.log("diametroAgujeroH1: " +diametroAgujeroH1.value +"nivelH1"+ nivelH1.value +" alturaAgujeroH1"+ alturaAgujeroH1.value);
+
+function starSimulation(){
+    let altura = nivelH1-alturaAgujeroH1;
+    volumenTanque1 = calculateVolumen(diametroH1,altura)
+    console.log(`Volumen: ${volumenTanque1}`);
+    let velocidadTanque1 = calculateVelocidad(altura);
+    let caudalTanque1 = calculateCaudal(diametroAgujeroH1,velocidadTanque1);
+    volumenTanque1 = volumenTanque1 - caudalTanque1;
+    altura = calculateAltura(diametroH1, volumenTanque1);
+    nivelH1 = altura + alturaAgujeroH1;
+    animatePaint(tanque1,0,alturaH1,diametroH1, diametroAgujeroH1, alturaAgujeroH1, nivelH1);
+
+    if(altura>0){
+        console.log("sigo vivo "+altura);
+    }else{
+        console.log(`altura ${altura} caudal ${caudalTanque1} volumenTanque1 ${volumenTanque1}`);
+        clearInterval(intervalo);
+    }
 
 }
+
+
+
+const calculateVelocidad = function (altura){
+    return Math.sqrt(2*9.8*altura)
+}
+
+const calculateCaudal = function(diametroAgujero, velocidad){
+    return (Math.pow(diametroAgujero/2,2)*Math.PI)*velocidad; 
+}
+
+const calculateVolumen = function(diametro, altura){
+    return Math.pow(diametro/2,2)*Math.PI*altura;
+}
+
+const calculateAltura = function(diametro, volumen){
+    return volumen/(Math.pow(diametro/2,2)*Math.PI);
+}
+
 
 function calculateCaudal1(raudioAgujeroH1,h1n1,radioH1){
     let caudal = 0.6 * (Math.PI* Math.pow(raudioAgujeroH1,2)* Math.sqrt(2*9.8*(h1n1)));
-    console.log("caudal: " + caudal);
+    //console.log("caudal: " + caudal);
+    resultados.innerHTML+="caudal: " + caudal;
     calculateVolumen1(radioH1,h1n1,caudal)
 }
 
 function calculateVolumen1(radioH1,h1n1,caudal){
     let volumenTanque1 = Math.PI * Math.pow(radioH1,2) * h1n1;
-    console.log("volumen: " + volumenTanque1);
+    //console.log("volumen: " + volumenTanque1);
+    resultados.innerHTML+="volumen: " + volumenTanque1;
     let volumenTange1Final = volumenTanque1 - caudal;   
     getHeight(volumenTange1Final,volumenTanque1, h1n1)
 }
 
 function getHeight(volumenF,volumen,h1n1){
     let height = (volumenF/volumen)*h1n1;
-    console.log("altura = " +height);
+    //console.log("altura = " +height);
+    resultados.innerHTML+="altura = " +height;
     return height;
 }
 
+function animatePaint(tanque, desplazamiento, altura, diametro, diametroAgujero, alturaAgujero, nivelAgua){
+    tanque.width = 300;
+    drawTanque(tanque,desplazamiento, altura, diametro, diametroAgujero, alturaAgujero, nivelAgua);
+}
 
 
-drawTanque(tanque1,0);
-drawTanque(tanque2,50);
+function rePaint(){
+        tanque1.width = 300;
+        tanque2.width = 300;
+    if(alturaH1.value != ""){
+        drawTanque(tanque1,0,parseFloat(alturaH1.value),parseFloat(diametroH1.value),parseFloat(diametroAgujeroH1.value),parseFloat(alturaAgujeroH1.value),parseFloat(nivelH1.value));
+        drawTanque(tanque2,50,parseFloat(alturaH2.value),parseFloat(diametroH2.value),parseFloat(diametroAgujeroH2.value),parseFloat(alturaAgujeroH2.value),parseFloat(nivelH2.value));
+    }else{
+        drawTanque(tanque1,0); 
+        drawTanque(tanque2,50);
+    }
+    
+}
+
+rePaint();
+
+
+
 
